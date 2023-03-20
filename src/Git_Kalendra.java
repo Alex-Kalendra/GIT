b91c95d87614172c3104344bed2ac5aa7d883e66
Alekss <aleks.kalendra12345@gmail.com>
2023-03-20 16:03:34 +0200

    Lokālajā aZarā papildināta programma

--- src/Git_Kalendra.java
+++ src/Git_Kalendra.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Git_Kalendra {
 
 	public static void main(String[] args) {
-	int skaitlis, reizes;
+	int skaitlis, reizes,lielakais =0;
 	Random rand = new Random();
 	Scanner scan = new Scanner(System.in);
 	do{
@@ -14,7 +14,10 @@ public class Git_Kalendra {
 	for(int i=1; i<=reizes; i++){
 	skaitlis = rand.nextInt(6)+1;
 	System.out.println("Uzkrita sakitlis "+skaitlis);
+	if(lielakais<skaitlis)
+		lielakais = skaitlis;
 		}
+	System.out.println("Lielakais metienos ir "+lielakais);
 		scan.close();
 	
 	
